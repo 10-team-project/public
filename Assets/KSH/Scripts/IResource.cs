89734400d2e55638b3b2356aa6c4caf50d46f120
76sohyun <conut1119@gmail.com>
2025-06-20 16:00:24 +0900

    [Feat] IResource 인터페이스, Resource 스크립트 구현

--- Assets/KSH/Scripts/IResource.cs
+++ Assets/KSH/Scripts/IResource.cs
@@ -2,7 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IResource
+namespace KSH
 {
-    
+    public interface IResource
+    {
+        float Max { get; }//최대 자원 양
+        float Cur { get; } //현재 자원 양
+
+        void Decrease(float amount); //자원 감소
+        void Increase(float amount); //자원 증가
+    }
 }
