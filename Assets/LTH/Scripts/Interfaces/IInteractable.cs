6ae67d49aced258817d4ea26b9786d7eea0db007
AsiaticRicefish <125473806+AsiaticRicefish@users.noreply.github.com>
2025-06-20 17:31:05 +0900

    [Feat] 상호작용 인터페이스 구현

---
+++ Assets/LTH/Scripts/Interfaces/IInteractable.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LTH;
+
+namespace LTH
+{
+    public interface IInteractable
+    {
+        public void Interact();
+    }
+}
+
+
+
