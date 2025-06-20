7dbfda666aacd40137a80b32da06159c7ead6e4a
76sohyun <conut1119@gmail.com>
2025-06-20 23:19:00 +0900

    [Refactor] ITickableResource, HP, ResourcceDegenator 코드 수정

--- Assets/KSH/Scripts/PlayerStat/HP.cs
+++ Assets/KSH/Scripts/PlayerStat/HP.cs
@@ -8,7 +8,7 @@ namespace KSH
     public class HP : MonoBehaviour
     {
         [Header("Assign a script")]
-        [SerializeField] private Resource resource;
+        [SerializeField] private ResourceDegenerator resourceDegenerator;
         [SerializeField] private Hunger hunger;
         [SerializeField] private Thirsty thirsty;
         [SerializeField] private Fatigue fatigue;
@@ -16,34 +16,37 @@ namespace KSH
         [SerializeField] private float hungerLimit;
         [SerializeField] private float thirstyLimit;
         [SerializeField] private float fatigueLimit;
+        [Header("HP Decay Interval")]
+        [SerializeField] private float decayTimeFromHunger;
+        [SerializeField] private float decayTimeFromThirsty;
+        [SerializeField] private float decayTimeFromFatigue;
         [Header("HP Drain Based on Resource Limits")]
         [SerializeField] private float HpLossFromHunger;
         [SerializeField] private float HpLossFromThirsty;
         [SerializeField] private float HpLossFromFatigue;
         
-        public Resource Resource => resource;
         public Hunger Hunger => hunger;
         public Thirsty Thirsty => thirsty;
         public Fatigue Fatigue => fatigue;
 
         private void Start()
         {
-            resource.Cur = resource.Max; //체력 초기화
+            resourceDegenerator.Resource.Cur = resourceDegenerator.Resource.Max; //체력 초기화
         }
 
         private void HealthDecay() //Hunger, Thirsty, Fatigue의 정해진 수치 이하가 되면 체력 떨어짐
         {
             if (hunger.HungerCur <= hungerLimit) // Hunger의 Cur이 일정 수준 내려가면
             {
-                resource.Decrease(HpLossFromHunger); //HP 깎임   
+                resourceDegenerator.ResourceTick(HpLossFromHunger, decayTimeFromHunger); // 몇초마다 HP 깎임   
             }
             else if (thirsty.ThirstyCur <= thirstyLimit) // Thirsty의 Cur이 일정 수준 내려가면
             {
-                resource.Decrease(HpLossFromThirsty); //HP 깎임
+                resourceDegenerator.ResourceTick(HpLossFromThirsty, decayTimeFromThirsty); //몇초마다 HP 깎임
             }
             else if (fatigue.FatigueCur <= fatigueLimit) // Fatigue의 Cur이 일정 수준 내려가면
             {
-                resource.Decrease(HpLossFromFatigue); //HP 깎임
+                resourceDegenerator.ResourceTick(HpLossFromFatigue, decayTimeFromFatigue); //몇초마다 HP 깎임
             }
         }
     }
--- Assets/KSH/Scripts/Resource/ResourceDegenerator.cs
+++ Assets/KSH/Scripts/Resource/ResourceDegenerator.cs
@@ -10,7 +10,7 @@ namespace KSH
         [Header("Decreases resource amount over time")]
         [Header("Resource Decay")]
         [SerializeField] private float decayTime; // 줄어들 시간(몇초마다 줄어들지)
-        [SerializeField] private float resourceAmount; //배고픔 양
+        [SerializeField] private float resourceAmount; //줄어들 자원 양
         [SerializeField] private bool decaying = true;
         
         [Header("Resource")]
@@ -23,13 +23,15 @@ namespace KSH
 
         private float timer = 0f; //초기 시간
         
-        public void ResourceTick()
+        public void ResourceTick(float amount, float decreaseTime)
         {
+            amount = resourceAmount;
+            decreaseTime = decayTime;
             timer += Time.deltaTime;
-            if (timer >= DecayTime && Decaying) // 만약 시간이 설정된 시간보다 크거나 같고 true이면
+            if (timer >= decreaseTime && decaying) // 만약 시간이 설정된 시간보다 크거나 같고 true이면
             {
                 timer = 0f; //다시 초기화
-                resource.Decrease(resourceAmount); //증가
+                resource.Decrease(amount); //감소
             }
         }
     }
