3b6a15941ec954b7a3fe42e64bbd7288568c561c
bart <bartshin@icloud.com>
2025-06-26 11:08:53 +0900

    [Feat] 상호작용을 통해 scene을 이동시키는 오브젝트

---
+++ Assets/SHG/Scripts/Map/MapGate.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using EditorAttributes;
+using UnityEngine.SceneManagement;
+using KSH;
+
+namespace SHG
+{
+  [RequireComponent (typeof(InteractUI))]
+  public class MapGate : MonoBehaviour, IInteractable
+  {
+    InteractUI interactUI;
+    bool isPresentingUI;
+    bool isMoved;
+    public Action<string> OnMove;
+    [SerializeField, FilePath(filters: "unity")] [Validate("Scene must in build settings", nameof(IsInvalidScene), MessageMode.Error, buildKiller: true)]
+    string sceneToMove;
+
+    void Awake()
+    {
+      this.isPresentingUI = false;
+      this.interactUI = this.GetComponent<InteractUI>();
+    }
+
+    void OnEnable()
+    {
+      this.isMoved = false;
+      this.interactUI.OnInteract += this.OnChangePresting;
+    }
+
+    void OnDisable()
+    {
+      this.isPresentingUI = false;
+      this.interactUI.OnInteract -= this.OnChangePresting;
+    }
+
+    void OnChangePresting(bool isPresenting)
+    {
+      this.isPresentingUI = isPresenting;
+    }
+
+    public void Interact()
+    {
+      if (this.isPresentingUI && !this.isMoved) {
+        this.isMoved = true;
+        this.OnMove?.Invoke(this.sceneToMove); 
+      }
+    }
+
+    protected bool IsInvalidScene()
+    {
+      if (this.sceneToMove.Replace(" ", "").Length == 0) {
+        return (true);
+      }
+      #if UNITY_EDITOR
+      var index = SceneUtility.GetBuildIndexByScenePath(this.sceneToMove);
+      if (index == -1) {
+        return (true);
+      }
+      #endif
+      return (false);
+    }
+  }
+}
