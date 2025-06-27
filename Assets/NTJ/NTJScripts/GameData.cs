c929b2b88696b2a528041f58ec459d82e38a4780
ro9ro9 <101381135+ro9ro9@users.noreply.github.com>
2025-06-27 17:35:36 +0900

    [feat]통합 세이브 스크립트 구조 변경중

--- Assets/NTJ/NTJScripts/GameData.cs
+++ Assets/NTJ/NTJScripts/GameData.cs
@@ -7,14 +7,13 @@ namespace NTJ
     [System.Serializable]
     public class GameData
     {
-        public int savedDay;
-        public float playerHP;
-        public float maxHP;
+        public float hp;
+        public float hunger;
+        public float thirst;
+        public float fatigue;
+        public int day;
 
-        // 플레이어 위치 저장
-        public float[] playerPosition = new float[3]; // x, y, z 좌표
-
-        // 인벤토리 (아이템 이름 리스트)
-        public List<string> inventoryItems = new List<string>();
+        public List<string> inventoryItemIDs = new(); // ScriptableObject 이름 또는 고유 ID
+        public List<string> quickSlotItemIDs = new();
     }
 }
