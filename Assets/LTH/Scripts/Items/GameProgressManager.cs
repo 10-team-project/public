7698475597086775fb9250ee51124a15e986a45d
AsiaticRicefish <125473806+AsiaticRicefish@users.noreply.github.com>
2025-07-04 17:10:29 +0900

    [Feat] 아이템 소환 조건 추가

--- Assets/LTH/Scripts/Items/GameProgressManager.cs
+++ Assets/LTH/Scripts/Items/GameProgressManager.cs
@@ -16,30 +16,38 @@ namespace LTH
 
     public class GameProgressManager : SingletonBehaviour<GameProgressManager>
     {
-        public bool HasCraftedRadio { get; private set; }
+        private HashSet<string> usedItemFlags = new();
+        private HashSet<string> obtainedItemFlags = new();
 
-        private HashSet<string> usedOneTimeItems = new();
-
-        public void OnRadioCrafted()
-        {
-            HasCraftedRadio = true;
-        }
-
+        // 사용 처리 (ex. 책, 열쇠 등 1회성 아이템)
         public void MarkItemAsUsed(string id)
         {
             if (!string.IsNullOrEmpty(id))
-                usedOneTimeItems.Add(id);
+                usedItemFlags.Add(id);
         }
 
-        public bool IsOneTimeItemUsed(string id)
+        public bool IsItemUsed(string id)
         {
-            return usedOneTimeItems.Contains(id);
+            return usedItemFlags.Contains(id);
         }
 
-        // 세이브/로드 연동
-        public List<string> GetUsedItemList() => new(usedOneTimeItems);
-        public void LoadUsedItemList(List<string> list) => usedOneTimeItems = new(list);
-        public void SetRadioCrafted(bool crafted) => HasCraftedRadio = crafted;
+        // 획득 처리 (ex. 라디오를 제작)
+        public void MarkItemAsObtained(string id)
+        {
+            if (!string.IsNullOrEmpty(id))
+                obtainedItemFlags.Add(id);
+        }
+
+        public bool IsItemObtained(string id)
+        {
+            return obtainedItemFlags.Contains(id);
+        }
+
+        // 세이브/로드 연동용
+        public List<string> GetUsedItemList() => new(usedItemFlags);
+        public void LoadUsedItemList(List<string> list) => usedItemFlags = new(list);
+
+        public List<string> GetObtainedItemList() => new(obtainedItemFlags);
+        public void LoadObtainedItemList(List<string> list) => obtainedItemFlags = new(list);
     }
 }
-
---
+++ Assets/LTH/Scripts/Items/Test/TestSpawn.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using LTH;
+using UnityEngine;
+
+namespace LTH
+{
+    public class TestSpawn : MonoBehaviour
+    {
+        public ProbabilityItemSpawner spawner;
+
+        [ContextMenu("Spawn All Items")]
+        public void SpawnAll()
+        {
+            if (spawner != null)
+            {
+                spawner.SpawnAll();
+                Debug.Log("아이템 스폰 완료");
+            }
+            else
+            {
+                Debug.LogWarning("spawner가 연결되어 있지 않습니다.");
+            }
+        }
+
+        [ContextMenu("Use Book")]
+        public void UseBook()
+        {
+            GameProgressManager.Instance.MarkItemAsUsed("BookTest1");
+            Debug.Log("책 사용 처리됨 → 다시 스폰되지 않음");
+        }
+
+        [ContextMenu("Craft Radio")]
+        public void CraftRadio()
+        {
+            GameProgressManager.Instance.MarkItemAsObtained("Radio");
+            Debug.Log("라디오 제작 완료 처리됨 (Radio 획득됨)");
+        }
+
+        [ContextMenu("Reset Progress")]
+        public void ResetProgress()
+        {
+            GameProgressManager.Instance.LoadUsedItemList(new());
+            GameProgressManager.Instance.LoadObtainedItemList(new());
+            Debug.Log("게임 진행 상태 초기화");
+        }
+    }
+}
